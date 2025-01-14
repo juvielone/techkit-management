1ea8c2d6b5c68e8d4e7f4733894e23ae6c35a8e7
Juvielone Lagos <62714904+juvielone@users.noreply.github.com>
2025-01-14 17:35:45 +0800

    Initial Commit
    Setup  and install shadcn sidebars

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,13 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+const AppSidebar = () => {
+  return <div>AppSidebar</div>;
+};
+
+export default AppSidebar;
